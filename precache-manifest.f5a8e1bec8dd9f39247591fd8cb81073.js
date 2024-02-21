d885dcb2ca2b84ec314750101414435f014db8a9
Neelam <neelam.shah@capgemini.com>
2024-02-21 13:10:27 +0100

    Updates

---
+++ precache-manifest.f5a8e1bec8dd9f39247591fd8cb81073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca501a8a5d75677206568c6e664a454",
+    "url": "/file-upload-azure/index.html"
+  },
+  {
+    "revision": "f64cf14ae0b8a0e0be14",
+    "url": "/file-upload-azure/static/css/main.6ebf534c.chunk.css"
+  },
+  {
+    "revision": "13333c1a984a0e3d4c71",
+    "url": "/file-upload-azure/static/js/2.2337e4ef.chunk.js"
+  },
+  {
+    "revision": "f64cf14ae0b8a0e0be14",
+    "url": "/file-upload-azure/static/js/main.8799a423.chunk.js"
+  },
+  {
+    "revision": "2012a1d02e5b4058a7e3",
+    "url": "/file-upload-azure/static/js/runtime-main.87cdb06a.js"
+  }
+]);
