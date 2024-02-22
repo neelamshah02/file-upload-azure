8a2879e5533535bfa86d4650ae33e2f0c5ad502e
Neelam <neelam.shah@capgemini.com>
2024-02-22 08:41:24 +0100

    remove log

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import ContainerList from './azure-storage/components/ContainerList';
-import InputFile from './azure-storage/components/InputFile';
-import ItemsDeleted from './azure-storage/components/ItemsDeleted';
-import ItemsDownloaded from './azure-storage/components/ItemsDownloaded';
-import ItemsList from './azure-storage/components/ItemsList';
-import ItemsUploaded from './azure-storage/components/ItemsUploaded';
-import SelectedContainer from './azure-storage/components/SelectedContainer';
-import Header from './layout/Header';
+import ContainerList from "./azure-storage/components/ContainerList";
+import InputFile from "./azure-storage/components/InputFile";
+import ItemsDeleted from "./azure-storage/components/ItemsDeleted";
+import ItemsDownloaded from "./azure-storage/components/ItemsDownloaded";
+import ItemsList from "./azure-storage/components/ItemsList";
+import ItemsUploaded from "./azure-storage/components/ItemsUploaded";
+import SelectedContainer from "./azure-storage/components/SelectedContainer";
+import Header from "./layout/Header";
 import "./test.css";
 
 interface profileView {
@@ -14,52 +14,54 @@ interface profileView {
 }
 // text
 const App: React.FC = () => {
-  const [profile, setProfile] = useState<profileView>({name:"VENDOR"});
+  const [profile, setProfile] = useState<profileView>({ name: "VENDOR" });
   const UserContext = React.createContext<profileView>({
-    name: profile.name
+    name: profile.name,
   });
-  const onProfileClick = (profileView:string) => {
+  const onProfileClick = (profileView: string) => {
     if (profileView === "VENDOR") {
-      setProfile({name:"FORVARSMATERIELL"});
+      setProfile({ name: "FORVARSMATERIELL" });
       console.log(profile);
     } else {
-      setProfile({name:"VENDOR"});
-      console.log(profile);
+      setProfile({ name: "VENDOR" });
     }
   };
-      return(
-  <>
-    <button className="button-21" onClick={() => onProfileClick(profile.name)}>{profile.name==="VENDOR"?("SUPPLIER"):(profile.name)}</button>
-    <Header/>
+  return (
+    <>
+      <button
+        className="button-21"
+        onClick={() => onProfileClick(profile.name)}
+      >
+        {profile.name === "VENDOR" ? "SUPPLIER" : profile.name}
+      </button>
+      <Header />
 
-    <hr />
+      <hr />
       <UserContext.Provider value={profile}>
-        <ContainerList user={profile.name}/>
+        <ContainerList user={profile.name} />
       </UserContext.Provider>
-    <hr />
-    <SelectedContainer className="container">
-        {profile.name !=="VENDOR"?(<div></div>):(<InputFile />)}
+      <hr />
+      <SelectedContainer className="container">
+        {profile.name !== "VENDOR" ? <div></div> : <InputFile />}
         <UserContext.Provider value={profile}>
-        {profile.name!=="VENDOR"?(
+          {profile.name !== "VENDOR" ? (
             <>
-              <ItemsList user={profile.name}/>
+              <ItemsList user={profile.name} />
               <div className="item-details">
                 {/* <ItemsUploaded/>         */}
                 <ItemsDownloaded />
                 <ItemsDeleted />
               </div>
             </>
-          ):(
+          ) : (
             <div className="item-details-1">
-              <ItemsUploaded/>
+              <ItemsUploaded />
             </div>
           )}
         </UserContext.Provider>
-    </SelectedContainer>
-  </>
-);
-}
+      </SelectedContainer>
+    </>
+  );
+};
 
 export default App;
-
-
