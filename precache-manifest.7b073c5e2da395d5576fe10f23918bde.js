b4aa47860ebf93944c84af1f7b38259230b600e3
Neelam <neelam.shah@capgemini.com>
2024-02-23 12:07:31 +0100

    Updates

--- precache-manifest.7b073c5e2da395d5576fe10f23918bde.js
+++ precache-manifest.7b073c5e2da395d5576fe10f23918bde.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f59cb2a9aac759f09866a1de83bd8291",
+    "revision": "febf6f8467280dd88da8c69f3020ce1a",
     "url": "/file-upload-azure/index.html"
   },
   {
-    "revision": "569e4e818a12922a510b",
-    "url": "/file-upload-azure/static/css/main.d82b5980.chunk.css"
+    "revision": "19cb6e2d431ad8bcf01a",
+    "url": "/file-upload-azure/static/css/main.9f33d63c.chunk.css"
   },
   {
     "revision": "13333c1a984a0e3d4c71",
     "url": "/file-upload-azure/static/js/2.2337e4ef.chunk.js"
   },
   {
-    "revision": "569e4e818a12922a510b",
-    "url": "/file-upload-azure/static/js/main.c1409f4c.chunk.js"
+    "revision": "19cb6e2d431ad8bcf01a",
+    "url": "/file-upload-azure/static/js/main.622265dc.chunk.js"
   },
   {
     "revision": "2012a1d02e5b4058a7e3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/file-upload-azure/precache-manifest.6c4c2e03a20d931d6005a00165ab9cc0.js"
+  "/file-upload-azure/precache-manifest.7b073c5e2da395d5576fe10f23918bde.js"
 );
 
 self.addEventListener('message', (event) => {
